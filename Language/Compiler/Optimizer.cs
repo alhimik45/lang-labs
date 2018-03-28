cb481f407c66996e17372a782825e12145296f22
a.kolpakov <a.kolpakov@skbkontur.ru>
2018-03-28 20:59:43 +0500

    all good but not best

--- Language/Compiler/Optimizer.cs
+++ Language/Compiler/Optimizer.cs
@@ -23,6 +23,7 @@ namespace Language.Compiler
             Operation.Lshift,
             Operation.Rshift,
             Operation.Push,
+            Operation.Jz,
         };
 
         private readonly Operation[] splitOps =
@@ -43,17 +44,20 @@ namespace Language.Compiler
             return splitted.SelectMany(ConstSubstitution).ToList();
         }
 
-        private List<List<Triad>> SplitLinear()
+        private List<List<(Triad, int)>> SplitLinear()
         {
-            var res = new List<List<Triad>>();
-            var ll = new List<Triad>();
-            foreach (var triad in ir)
+            var res = new List<List<(Triad, int)>>();
+            var ll = new List<(Triad, int)>();
+            var dests = ir.Where(tr => tr.Operation == Operation.Jmp).Select(tr => (int)tr.Arg1)
+                .Union(ir.Where(tr => tr.Operation == Operation.Jz).Select(tr => (int)tr.Arg2));
+            for (var i = 0; i < ir.Count; i++)
             {
-                ll.Add(triad);
-                if (splitOps.Contains(triad.Operation))
+                var triad = ir[i];
+                ll.Add((triad, i));
+                if (splitOps.Contains(triad.Operation) || dests.Contains(i+1))
                 {
                     res.Add(ll);
-                    ll = new List<Triad>();
+                    ll = new List<(Triad, int)>();
                 }
             }
 
@@ -61,12 +65,13 @@ namespace Language.Compiler
             return res;
         }
 
-        private List<Triad> ConstSubstitution(List<Triad> code)
+        private List<Triad> ConstSubstitution(List<(Triad, int)> code)
         {
             var constValues = new Dictionary<IResult, ConstResult>();
+            var res = new List<Triad>();
             for (var i = 0; i < code.Count; i++)
             {
-                var triad = code[i];
+                var triad = code[i].Item1;
                 if (triad.Operation == Operation.Assign)
                 {
                     if (constValues.ContainsKey(triad.Arg2))
@@ -90,19 +95,19 @@ namespace Language.Compiler
                         switch (triad.Arg2)
                         {
                             case SemType.Int:
-                                val = (int) tr.Value;
+                                val = (int)tr.Value;
                                 break;
                             case SemType.LongLongInt:
-                                val = (long) tr.Value;
+                                val = (long)tr.Value;
                                 break;
                             case SemType.Char:
-                                val = (char) tr.Value;
+                                val = (char)tr.Value;
                                 break;
                             default:
                                 throw new ArgumentOutOfRangeException(nameof(tr.Value));
                         }
 
-                        constValues[TriadResult.Of(i, triad.Arg2)] = ConstResult.Of(val);
+                        constValues[TriadResult.Of(code[i].Item2, triad.Arg2)] = ConstResult.Of(val);
                         triad.Operation = Operation.Nop;
                         triad.Arg1 = triad.Arg2 = null;
                     }
@@ -115,7 +120,7 @@ namespace Language.Compiler
                         triad.Arg1 = constValues[triad.Arg1];
                     }
 
-                    if (triad.Arg2 != null && constValues.ContainsKey(triad.Arg2))
+                    if (triad.Arg2 != null && triad.Arg2 is IResult && constValues.ContainsKey(triad.Arg2))
                     {
                         triad.Arg2 = constValues[triad.Arg2];
                     }
@@ -135,13 +140,13 @@ namespace Language.Compiler
                                     val = ConstResult.Of(cr1.Value - val2);
                                     break;
                                 case Operation.Mul:
-                                    val = ConstResult.Of(cr1.Value * val2);
+                                    val = ConstResult.Of(cr1.Value*val2);
                                     break;
                                 case Operation.Div:
-                                    val = ConstResult.Of(cr1.Value / val2);
+                                    val = ConstResult.Of(cr1.Value/val2);
                                     break;
                                 case Operation.Mod:
-                                    val = ConstResult.Of(cr1.Value % val2);
+                                    val = ConstResult.Of(cr1.Value%val2);
                                     break;
                                 case Operation.And:
                                     val = ConstResult.Of(cr1.Value & val2);
@@ -162,6 +167,7 @@ namespace Language.Compiler
                                     val = ConstResult.Of(~cr1.Value);
                                     break;
                                 case Operation.Push:
+                                case Operation.Jz:
                                     break;
                                 default:
                                     throw new ArgumentOutOfRangeException(nameof(triad.Operation));
@@ -169,16 +175,17 @@ namespace Language.Compiler
 
                             if (val != null)
                             {
-                                constValues[TriadResult.Of(i, val.Type)] = val;
+                                constValues[TriadResult.Of(code[i].Item2, val.Type)] = val;
                                 triad.Operation = Operation.Nop;
                                 triad.Arg1 = triad.Arg2 = null;
                             }
                         }
                     }
                 }
+                res.Add(triad);
             }
 
-            return code;
+            return res;
         }
     }
 }
