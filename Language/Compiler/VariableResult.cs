6cea624df4d31fbb7f7f00db15d09f85719c0bef
a.kolpakov <a.kolpakov@skbkontur.ru>
2018-03-28 19:10:47 +0500

    fix

--- Language/Compiler/VariableResult.cs
+++ Language/Compiler/VariableResult.cs
@@ -16,5 +16,24 @@ namespace Language.Compiler
         {
             return $"{Var.FullName}";
         }
+        
+        protected bool Equals(VariableResult other)
+        {
+            return Equals(Var, other.Var);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            return obj.GetType() == GetType() && Equals((VariableResult)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Var != null ? Var.GetHashCode() : 0;
+        }
     }
 }
