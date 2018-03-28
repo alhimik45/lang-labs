cf24ee00dc77c2c2cf5e4d0a078a5e672b8d8dfa
alhimik45 <mail@alhimik.me>
2018-03-28 22:14:16 +0700

    add not work

--- Language/Compiler/Optimizer.cs
+++ Language/Compiler/Optimizer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Language.Analyzer;
 
@@ -19,6 +20,8 @@ namespace Language.Compiler
             Operation.Or,
             Operation.And,
             Operation.Not,
+            Operation.Lshift,
+            Operation.Rshift,
             Operation.Push,
         };
 
@@ -95,6 +98,47 @@ namespace Language.Compiler
                     if (triad.Arg2 != null && constValues.ContainsKey(triad.Arg2))
                     {
                         triad.Arg2 = constValues[triad.Arg2];
+                    }
+
+                    if (triad.Arg1 is ConstResult cr1)
+                    {
+                        if (triad.Arg2 == null || triad.Arg2 is ConstResult)
+                        {
+                            var val2 = triad.Arg2 is ConstResult cr2 ? cr2.Value : null;
+                            switch (triad.Operation)
+                            {
+                                case Operation.Add:
+                                    var val = ConstResult.Of(cr1.Value + val2);
+                                    constValues[TriadResult.Of(i, val.Type)] = val;
+                                    break;
+                                case Operation.Sub:
+                                    break;
+                                case Operation.Mul:
+                                    break;
+                                case Operation.Div:
+                                    break;
+                                case Operation.Mod:
+                                    break;
+                                case Operation.And:
+                                    break;
+                                case Operation.Or:
+                                    break;
+                                case Operation.Xor:
+                                    break;
+                                case Operation.Lshift:
+                                    break;
+                                case Operation.Rshift:
+                                    break;
+                                case Operation.Not:
+                                    break;
+                                case Operation.Push:
+                                    break;
+                                default:
+                                    throw new ArgumentOutOfRangeException(nameof(triad.Operation));
+                            }
+                            triad.Operation = Operation.Nop;
+                            triad.Arg1 = triad.Arg2 = null;
+                        }
                     }
                 }
             }
